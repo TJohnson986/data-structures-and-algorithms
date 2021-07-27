9064ffae52214741667ed2610dbd61968212da2e
Tyler Johnson <tyler@csdconsultants.us>
2021-07-26 23:20:10 -0700

    wrote third test, testing pop value off a stack

--- javascript/stack-and-queue/__tests__/stack-and-queue.test.js
+++ javascript/stack-and-queue/__tests__/stack-and-queue.test.js
@@ -15,16 +15,19 @@ describe('testing stack functionality', () => {
   test('push multiple values onto a stack', () => {
     stack.push(2);
     stack.push(3);
-    console.log(stack);
-    console.log('tests', stack.top.value, 'next', stack.top.next.value);
+    // console.log(stack);
+    // console.log('tests', stack.top.value, 'next', stack.top.next.value);
 
     expect(stack.top.value).toEqual(3);
     expect(stack.top.next.value).toEqual(2);
   });
 
-  // test('pop off the stack', () => {
+  test('pop off the stack', () => {
+    stack.pop();
+    // console.log('pop', stack);
 
-  // });
+    expect(stack.top.value).toEqual(2);
+  });
 
 
   // test('empty stack after multiple pops', () => {
