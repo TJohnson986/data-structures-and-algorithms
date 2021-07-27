673ec7b837005086a847fd836fc117e29837110f
Tyler Johnson <tyler@csdconsultants.us>
2021-07-26 17:24:12 -0700

    wrote second test, testing push of multiple values to a stack

--- javascript/stack-and-queue/__tests__/stack-and-queue.test.js
+++ javascript/stack-and-queue/__tests__/stack-and-queue.test.js
@@ -6,15 +6,21 @@ describe('testing stack functionality', () => {
 
   test('push onto a stack', () => {
     stack.push(1);
-    console.log('tests', stack.top.value);
+    // console.log('tests', stack.top.value);
 
     expect(stack.top.value).toEqual(1);
   });
 
 
-  // test('push multiple values onto a stack', () => {
+  test('push multiple values onto a stack', () => {
+    stack.push(2);
+    stack.push(3);
+    console.log(stack);
+    console.log('tests', stack.top.value, 'next', stack.top.next.value);
 
-  // });
+    expect(stack.top.value).toEqual(3);
+    expect(stack.top.next.value).toEqual(2);
+  });
 
   // test('pop off the stack', () => {
 
