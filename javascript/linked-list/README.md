9ba8d317e8d72f6b08a4314b84d598ead1c32f06
Tyler Johnson <tyler@csdconsultants.us>
2021-08-15 13:00:12 -0700

    completes code challenge 06, updates README

---
+++ javascript/linked-list/README.md
@@ -0,0 +1,10 @@
+# Code Challenges 05 - 08 -- Linked Lists
+
+## Challenge
+New implementation Linked Lists, adding nodes, appending nodes at a certain index, inserting before or after a certian value, etc.
+
+## Approach & Efficiency
+<!-- What approach did you take? Why? What is the Big O space/time for this approach? -->
+
+## API
+<!-- Description of each method publicly available in each of your trees -->
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -16,7 +16,6 @@ class LinkedList {
     let current = this.head;
 
     while (current !== null) {
-      // console.log(current.value);
       current = current.next;
     }
   }
@@ -24,34 +23,94 @@ class LinkedList {
   add(value) {
     let current = this.head;
     let previous = null;
-    while (current != null) {
+    while (current !== null) {
       previous = current;
       current = current.next;
     }
 
-    let newNode = new Node (value);
+    let newNode = new Node(value);
     previous.next = newNode;
   }
+
+  append(value, idx) {
+    let current = this.head;
+    let previous = null;
+    let newNode = new Node(value);
+
+    if (idx === 0) {
+      current.next = this.head;
+      this.head = newNode;
+    } else {
+      current = this.head;
+      let loc = 0;
+
+      while (loc < idx) {
+        loc++;
+        previous = current;
+        current = current.next;
+      }
+
+      newNode.next = current;
+      // console.log('current', current);
+      previous.next = newNode;
+    }
+  }
+
+  insertBefore(value, newValue) {
+    let current = this.head;
+    let previous = null;
+    let newNode = new Node(newValue);
+
+    while (value !== current.value && current.next !== null) {
+      previous = current;
+      current = current.next;
+    }
+
+    previous.next = newNode;
+    newNode.next = current;
+  }
+
+  insertAfter(value, newValue) {
+    let current = this.head;
+    let previous = null;
+    let newNode = new Node(newValue);
+
+    while (value !== current.value && current.next !== null) {
+      current = current.next;
+    }
+
+    // previous.next = current;
+    newNode.next = current.next;
+    current.next = newNode;
+  }
+
 }
 
 const list = new LinkedList();
-const A = new Node (5);
+const A = new Node(5);
 
 list.head = A;
 
 
-const B = new Node ('Tyler');
+const B = new Node('Tyler');
 
 list.head.next = B;
 
 
-const C = new Node (false);
+const C = new Node(false);
 
 list.head.next.next = C;
 
-console.log(JSON.stringify(list));
 
 list.add(10);
+
+list.append(30, 1);
+
+list.insertBefore(30, 15);
+
+list.insertAfter(15, 16);
+
+console.log(JSON.stringify(list));
 
 list.traverse();
 
